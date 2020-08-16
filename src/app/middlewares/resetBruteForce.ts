42489c64e9c45c74ad873d2f3a19e8cf4d65c28f
Felipe Barros <felipetech@tuta.io>
2020-08-16 17:12:20 -0300

    refactor: Fix warning
    Fix the warning telling that "req.brute.reset" can be undefined

--- src/app/middlewares/resetBruteForce.ts
+++ src/app/middlewares/resetBruteForce.ts
@@ -3,7 +3,9 @@ import { Response, NextFunction, Request } from "express";
 export default (req: Request, res: Response, next: NextFunction) => {
   // reset the failure counter so next time they log in they get 5 tries again
   // before the delays kick in
-  req.brute?.reset(() => {
-    return res.redirect('/'); // logged in, send them to the home page
-  });
+  if (req.brute && req.brute.reset) {
+    req.brute.reset(() => {
+      return res.redirect('/'); // logged in, send them to the home page
+    });
+  }
 }
